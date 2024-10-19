57c9b99b0bac9a0431a3deb55e9a8967a75cfa2d
veera reddy inakollu <ivreddy1422@gmail.com>
2024-10-19 12:27:33 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+#creating VPC
+resource "aws_vpc" "demovpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name="Demo VPC"
+  }
+}
