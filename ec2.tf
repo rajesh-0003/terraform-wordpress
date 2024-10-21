4694a0cb78aa6c3f19902286f7d41f5c3aa5939c
veera reddy inakollu <ivreddy1422@gmail.com>
2024-10-21 22:52:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 #Creating 1st EC2 instance
 resource "aws_instance" "instance-wordpress" {
-  ami = "ami-0a5c3558529277641"
+  ami = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
-  key_name = "veera"
+  key_name = "anil"
   subnet_id = aws_subnet.public_sb_1.id
   vpc_security_group_ids = [aws_security_group.security1.id]
   associate_public_ip_address = true
